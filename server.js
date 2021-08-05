07464192a34e49a1d3379949907cfdfc91f16e54
Nurgisa A <apbaz.nrgisa@gmail.com>
2021-08-05 17:14:43 +0500

    server settings

--- server.js
+++ server.js
@@ -7,22 +7,3 @@ app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
 
-const http = require('http')
-const fs = require('fs')
-const httpPort = port
-
-http.createServer((req, res) => {
-  fs.readFile('index.html', 'utf-8', (err, content) => {
-    if (err) {
-      console.log('We cannot open "index.html" file.')
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
-
-    res.end(content)
-  })
-}).listen(httpPort, () => {
-  console.log('Server listening on: http://localhost:%s', httpPort)
-})
